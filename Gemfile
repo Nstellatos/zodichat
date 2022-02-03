43873b8ac74e08c77d28f9ec9dfc0e46120c8c8c
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-02-03 16:49:30 -0500

    add faker gem and seed db

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jquery-rails'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 gem 'devise'
+gem 'faker'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def seed_users
+  user_id = 0
+  10.times do 
+    User.create(
+      name: "test#{user_id}",
+      email: "test#{user_id}@test.com",
+      password: '123456',
+      password_confirmation: '123456'
+    )
+    user_id = user_id + 1
+  end
+end
+
+
+def seed_categories
+  sun_sign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
+  rising_sign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
+  moon_sign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
+
+  sun_sign.each do |name|
+    Category.create(branch: 'sun sign', name: name)
+  end
+
+  rising_sign.each do |name|
+    Category.create(branch: 'rising sign', name: name)
+  end
+
+  moon_sign.each do |name|
+    Category.create(branch: 'moon sign', name: name)
+  end
+end
+
+def seed_posts
+  categories = Category.all
+
+  categories.each do |category|
+    5.times do
+      Post.create(
+        title: Faker::Lorem.sentences[0], 
+        content: Faker::Lorem.sentences[0], 
+        user_id: rand(1..9), 
+        category_id: category.id
+      )
+    end
+  end
+end
+
+seed_users
+seed_categories
+seed_posts
