a203de9b675b1b5d3500f555efb55fdeb7a49716
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-02-04 17:24:32 -0500

    define new and create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
 def show
     @post = Post.find(params[:id])
 end
@@ -12,6 +13,20 @@ end
 def moon_sign
     posts_for_branch(params[:action])
 end
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end
 
 
 private
@@ -31,6 +46,10 @@ end
       branch: params[:action]
     }).call
   end
+  def post_params
+  params.require(:post).permit(:content, :title, :category_id)
+                       .merge(user_id: current_user.id)
+end
 
 
 end
