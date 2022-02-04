32e26989997da8060c0da02a56c604df3fbdc8a3
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-02-04 17:16:39 -0500

    Add posts from all branches in the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
     def index
-    @posts = Post.limit(5)
+    @sun_sign_posts = Post.by_branch('sun_sign').limit(8)
+    @rising_sign_posts = Post.by_branch('rising_sign').limit(8)
+    @moon_sign_posts = Post.by_branch('moon_sign').limit(8)
     end
     
 end
